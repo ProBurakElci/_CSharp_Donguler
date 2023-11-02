75add8ebe8df463f82598966fc2184636cd5b3d4
Burak Elçi <burakelci12@gmail.com>
2023-11-02 11:44:58 +0300

    Proje dosyası ekle.

---
+++ _3_Donguler/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace _3_Donguler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalistir = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalistir
+            // 
+            this.btnCalistir.Location = new System.Drawing.Point(81, 15);
+            this.btnCalistir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalistir.Name = "btnCalistir";
+            this.btnCalistir.Size = new System.Drawing.Size(169, 52);
+            this.btnCalistir.TabIndex = 0;
+            this.btnCalistir.Text = "DÖNGÜYÜ ÇALIŞTIR";
+            this.btnCalistir.UseVisualStyleBackColor = true;
+            this.btnCalistir.Click += new System.EventHandler(this.btnCalistir_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(81, 96);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 164);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCalistir);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalistir;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ _3_Donguler/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_Donguler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac;
+
+        private void btnCalistir_Click(object sender, EventArgs e)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    listBox1.Items.Add(i + "-Sukran");
+            //}
+            //------------------------------------------------------------------------------
+            int i = 0;
+            for (; i < 10; i++)
+            {
+                listBox1.Items.Add(i + "-Burak");
+            }
+            MessageBox.Show(i + "");
+            // eğer int i = 0 diye döngü içinde tanımlansaydı, bu i nin değeri sadece döngü içinde kullanılabilirdi. bunu dışarıda tanımlayarak, for scope dışında da kullanımını sağladık.
+            //---------------------------------------------------------------------------------
+            //int i;
+            //for (i = 2; i < 10; i++)
+            //{
+            //}
+            //--------------------------------------------------------------------------------
+            //int i = 2;
+            //for (; i < 10; )
+            //{
+            // listBox1.Items.Add(i + "-Ahmet");
+            // i++;
+            //}
+            // döngü değişkeninni değeri, for scope iinde değiştirilebilir.
+            //---------------------------------------------------------------------------------
+            //for (; ; )
+            //{
+            // listBox1.Items.Add("-Ahmet");
+            // // Bu döngü SONSUZ DÖNGÜDÜR.
+            //}
+            //MessageBox.Show("Test");
+            // NOT : Döngünün çalışması sona ermeden, döngüden sonraki komutlar ÇALIŞTIRILMAZ.
+            //------------------------------------------------------------------------------------
+            //for (int i = 0; i < 10; i += 1)
+            //{
+            //}
+            //for (int i = 0; i < 10; i = i + 1)
+            //{
+            //}
+            //for (int i = 0; i < 10; i += 5)
+            //{
+            //}
+            //for (int i = 0; i < 10; i = (i + 5) * 2 - 4)
+            //{
+            //}
+            //for (int i = 0; i > 2 && i < (i + 5) % 2; i++)
+            //{
+            //}
+            //for (int i = 0, k = 5, z = 15; i < 10 && k != 5 || z >= 4; i++, k -= 2)
+            //{
+            //    MessageBox.Show("Test");
+            //    z = i + k;
+            //}
+            //for (ulong i = 10; i < 10; i++)
+            //{
+            //}
+            //for (int i = 10; i > 0; i--)
+            //{
+            //}
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
